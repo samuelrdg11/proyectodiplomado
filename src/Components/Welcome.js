2a5a02daebafe126b85be17e0bdc701d2b69e6a0
TheRodry <rohesa09@gmail.com>
2023-05-11 08:54:13 -0500

    Commit Welcome

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -2,9 +2,106 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+      <div>
+        <head>
+  
+  <nav class="navbar navbar-light bg-light">
+      <b class="navbar-brand" href="#">
+        <img  src="libro3.png" width="40" height="40" class="d-inline-block align-top" alt="" />
+        BIENVENIDOS 
+      </b>
+    </nav>
+    </head>
+    <body>
+      <section class="container" >
+          <div class="row">
+              <div style="text-align: justify;" class="col-sm-12 col-md-8 col-lg-6 col-xxl-4">
+                  Bienvenidos a nuestra página web bibliotecaria, 
+                  donde podrás encontrar una gran variedad de recursos y 
+                  servicios para satisfacer tus necesidades de investigación,
+                  aprendizaje y entretenimiento.
+                  <br/>
+                  <br/>
+                  Nuestra biblioteca cuenta con una amplia colección de libros,
+                  revistas, periódicos y material multimedia, en diferentes formatos
+                  y en diversos idiomas, para que puedas encontrar fácilmente lo que necesitas.
+                  También ofrecemos acceso a bases de datos en línea, donde podrás
+                  acceder a información académica y científica de alta calidad, así 
+                  como recursos para el aprendizaje de idiomas y otras habilidades.
+              </div>
+              <div class="col-sm-12 col-md-8 col-lg-6 col-xxl-4">
+                  <img src="libros.png" class="rounded mx-auto d-block" height="300" width="500" alt="..."/>
+              </div>
+          </div>
+  <br/>
+  <br/>
+          <div class="container">
+              <div  class="row">
+                  <div class="col-sm-12 col-md-8 col-lg-6 col-xxl-4">
+                      <img src="libro2.png" class="rounded mx-auto d-block" height="300" width="500" alt="..."/>
+                  </div>
+                  
+                  <div style="text-align: justify;" class="col-sm-12 col-md-8 col-lg-6 col-xxl-4">
+                      Además de los recursos físicos y en línea, nuestra biblioteca
+                      cuenta con servicios especializados, como préstamo a domicilio, 
+                      asesoramiento bibliográfico, referencia en línea, talleres y actividades 
+                      para niños y adultos.
+                      <br/>
+                      
+                      En nuestra página web podrás explorar nuestro catálogo en línea,
+                      hacer reservas y renovaciones de tus préstamos, acceder a recursos 
+                      digitales y obtener información actualizada sobre los servicios y 
+                      eventos de la biblioteca.
+                      
+                      <br/>
+                      nuestra biblioteca es un lugar donde podrás encontrar todo 
+                      lo que necesitas para tu desarrollo personal y profesional, y nuestra 
+                      página web es la herramienta ideal para acceder a nuestros recursos y 
+                      servicios desde cualquier lugar y en cualquier momento.
+                  </div>
+                  
+              </div>
+          </div>
+  <br/>
+  <br/>
+          <div class="row">
+                  <div style="text-align: justify;" class="col-sm-12 col-md-8 col-lg-6 col-xxl-4">
+                      Una de las ventajas de las bibliotecas virtuales es que ofrecen acceso 
+                      a materiales de lectura desde cualquier lugar y en cualquier momento,
+                      siempre que se tenga una conexión a internet. Además, los usuarios
+                      pueden acceder a los materiales en varios formatos, lo que les permite
+                      leer en dispositivos electrónicos como teléfonos inteligentes, tabletas,
+                      lectores electrónicos, computadoras y otros dispositivos.
+                      <br/>
+                      <br/>
+                      Nuestra biblioteca cuenta con una amplia colección de libros,
+                      revistas, periódicos y material multimedia, en diferentes formatos
+                      y en diversos idiomas, para que puedas encontrar fácilmente lo que necesitas.
+                      También ofrecemos acceso a bases de datos en línea, donde podrás
+                      acceder a información académica y científica de alta calidad, así 
+                      como recursos para el aprendizaje de idiomas y otras habilidades.
+                  </div>
+                  <div class="col-sm-12 col-md-8 col-lg-6 col-xxl-4">
+                      
+                      <img src="libros4.png" class="rounded mx-auto d-block" height="300" width="500" alt="..."/>
+                  </div>
+          </div>
+      </section>
+      <footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
+          
+          <div class="container p-4"></div>
+          
+          <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+              ¡Te invitamos a explorar todo lo que nuestra biblioteca tiene para ofrecerte!
+          </div>
+          
+        </footer>
+      
+  
+      
+      
+    </body>
+  </div>
   )
 }
 

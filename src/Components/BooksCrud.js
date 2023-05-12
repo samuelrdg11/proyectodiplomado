001c90a1141fbdc8f4982c8fb08f1055fad89d6f
Samuel <samuelrdg11@gmail.com>
2023-05-12 08:59:49 -0500

    modulo de prestamo y devolucion

--- src/Components/BooksCrud.js
+++ src/Components/BooksCrud.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../firebase';
-import { Table } from 'react-bootstrap';
+import { Card, Modal, Form, Button } from 'react-bootstrap';
 import BookModal from './BookModal';
 import { useNavigate } from 'react-router-dom'
 import { auth } from '../firebase'
@@ -11,6 +11,8 @@ const BooksCrud = () => {
   const [editId, setEditId] = useState(null)
   const [search, setSearch] = useState('');
   const [user, setUser] = React.useState(null)
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
   const navigate = useNavigate()
 
   React.useEffect(() => {
@@ -48,51 +50,85 @@ const BooksCrud = () => {
   const handleCloseModal = () => {
     setShowModal(false);
   };
+  const closeModalConfirmation = () => {
+    setShowConfirmModal(false);
+  };
+
+  const handleBorrowBook = book => {
+    setSelectedBook(book);
+    setShowConfirmModal(true);
+  };
+  const returnBook = book => {
+    setSelectedBook(book);
+    setShowConfirmModal(true);
+  };
+
+  const handleConfirmBorrowBook = () => {
+    db.collection('books').doc(selectedBook.id).update({ disponibilidad: !selectedBook.disponibilidad });
+    setShowConfirmModal(false);
+  };
 
   return (
-    <div className='container'>
-      {
-        user && (
-          <>
-            <input
-              type="text"
-              className="inputFiltrar form-control"
-              placeholder="Titulo o autor del libro"
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <div className="container">
-              <button className='botonAñadir btn btn-sm' onClick={handleAddBook}>Agregar libro</button>
-              <Table className='tableBooks' responsive striped bordered hover variant="dark">
-                <thead>
-                  <tr>
-                    <th>Título</th>
-                    <th>Autor</th>
-                    <th>Genero</th>
-                    <th>Acciones</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {books.filter((book) => {
-                    return search.toLocaleLowerCase() === '' ? 
-                    book : book.autor.toLocaleLowerCase().includes(search.toLocaleLowerCase()) || book.titulo.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-                  }).map((book) => (
-                    <tr key={book.id}>
-                      <td>{book.titulo}</td>
-                      <td>{book.autor}</td>
-                      <td>{book.genero}</td>
-                      <td>
-                        <button className='botonEditar btn btn-sm' onClick={() => handleEditBook(book)}>Editar</button>
-                        <button className='botonEliminar btn btn-sm' onClick={() => handleDeleteBook(book.id)}>Eliminar</button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-              <BookModal showModal={showModal} handleCloseModal={handleCloseModal} editId={editId} />
-            </div>
-          </>
+    <>
+      <div className='container'>
+        <input
+          type="text"
+          className="inputFiltrar form-control"
+          placeholder="Titulo o autor del libro"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div>
+        <button className='botonAñadir btn btn-sm' onClick={handleAddBook}>Agregar libro</button>
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <BookModal onClose={handleCloseModal} editId={editId} />
+        </Modal>
+        {showConfirmModal && (
+          <Modal show={showConfirmModal} onHide={closeModalConfirmation}>
+            <Modal.Header closeButton>
+              <Modal.Title>{selectedBook && selectedBook.disponibilidad ? 'Confirmar préstamo' : 'Confirmar devolución'}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              ¿Estás seguro de que quieres {selectedBook && selectedBook.disponibilidad ? 'tomar prestado' : 'devolver'} el libro <strong>{selectedBook && selectedBook.titulo}</strong>?
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={closeModalConfirmation}>
+                Cancelar
+              </Button>
+              <Button variant="primary" onClick={handleConfirmBorrowBook}>
+                Confirmar
+              </Button>
+            </Modal.Footer>
+          </Modal>
         )}
-    </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          {books.filter((book) => {
+            return search.toLocaleLowerCase() === '' ?
+              book : book.autor.toLocaleLowerCase().includes(search.toLocaleLowerCase()) || book.titulo.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+          }).map((book) => (
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={book.base64Portada} />
+              <Card.Body>
+                <Card.Title>{book.titulo}</Card.Title>
+                <Card.Text>{book.autor}</Card.Text>
+                <Card.Text>{book.genero}</Card.Text>
+                <Card.Text><strong>{book.disponibilidad ? 'Disponible' : 'Prestado'}</strong> </Card.Text>
+                {book.disponibilidad ?
+                  <Card.Text> <Button variant="success" onClick={() => handleBorrowBook(book)}>Prestar</Button></Card.Text>
+                  :
+                  <Card.Text><Button variant="secondary" onClick={() => handleBorrowBook(book)}>Devolver</Button></Card.Text>
+                }
+                <button className='botonEditar btn btn-sm' onClick={() => handleEditBook(book)}>Editar</button>
+                <button className='botonEliminar btn btn-sm' onClick={() => handleDeleteBook(book.id)}>Eliminar</button>
+              </Card.Body>
+            </Card>
+          ))}
+          <BookModal showModal={showModal} handleCloseModal={handleCloseModal} editId={editId} />
+        </div >
+      </div>
+    </>
   );
 };
 
